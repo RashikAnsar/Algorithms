600d9cf6b31b2dd259e424d1f4e9f3ce93bbba89
RashikAnsar <sk.ansar46@gmail.com>
2018-11-04 23:09:11 +0530

    Linear search comments

--- Searching/linearSearch/linearSearch.js
+++ Searching/linearSearch/linearSearch.js
@@ -5,14 +5,18 @@
  * @return {number[] | -1 }
  */
 function linearSearch(arr, val) {
+  // Initialize an empty array to store indices
   let indices = [];
+  // loop over the array
   for (let i in arr) {
+    // check for equality and push indices into empty array
     if (arr[i] === val) {
       indices.push(i);
     }
   }
+  // return the indices array if it length is > 0
   if (indices.length) return indices;
-
+  // return -1 if value doesn't exist
   return -1;
 }
 
